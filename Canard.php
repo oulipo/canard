07a2c61b1ac203cbdd8527022cda025ab55b4039
laurentws <laurent@websuit.fr>
2018-01-15 21:32:58 +0100

    une bonne idée ?

--- Canard.php
+++ Canard.php
@@ -1,57 +1,71 @@
 <?php
 // Simulateur de canard...
 
-abstract class Canard {
+interface Volable {
+    public function vole();
+}
 
-    public function cancane() {
-        echo "coin coin\n";
-    }
+interface Cancanable { // néologisme !
+    public function cancane();
+}
+
+abstract class Canard {
 
     public function nage() {
         echo "Je nage...\n";
     }
 
-    public function vole() { // tous les canards volent désormais...
-        echo "Je vole...\n";
-    }
-
     abstract protected function affiche();
 }
 
-class Colvert extends Canard {
+class Colvert extends Canard implements Cancanable, Volable {
     public function affiche() {
         echo "Je suis un colvert\n";
     }
+    public function vole() {
+        echo "Je vole...\n";
+    }
+    public function cancane() {
+        echo "coin-coin\n";
+    }
 }
 
-class TeteRouge extends Canard {
+class TeteRouge extends Canard implements Cancanable, Volable {
     public function affiche() {
         echo "Je suis un fuligule à tête rouge\n";
     }
+    public function vole() {
+        echo "Je vole...\n";
+    }
+    public function cancane() {
+        echo "coin-coin\n";
+    }
 }
 
-// mais un autre développeur ajoute la classe CanardEnCaoutchouc...
-class CanardEnCaoutchouc extends Canard {
+class CanardEnCaoutchouc extends Canard implements Cancanable {
     public function affiche() {
         echo "Je suis un canard en caoutchouc\n";
     }
-    public function cancane() { // on override ici car ce type de canard ne cancane pas pas mais fait couic-couic...
+    public function cancane() { 
         echo "couic couic...\n";
+    }
+}
+
+class LeurreDeCanardEnBois extends Canard {
+    public function affiche() {
+        echo "Je suis un un faux canard\n";
     }
 }
 
 $c1 = new Colvert();
 $c2 = new TeteRouge();
-$c1->affiche();
-$c1->nage();
-$c2->affiche();
-$c2->cancane();
-
-// désormais les canards en caoutchouc peuvent voler...
 $c3 = new CanardEnCaoutchouc();
-$c3->vole();
-// L'héritage semble être une bonne idée pour réutiliser le code mais cela devient problématique la maintenance et l'évolution du projet devient plus compliquée...
-// on va devoir overrider vole() pour que ce canard ne vole pas.
+$c4 = new LeurreDeCanardEnBois();
 
-// idem si l'on doit ajouter une classe pour les leurres de canard en bois
-// celle-ci ne doit pas voler, ni cancaner...
+// Que pensez-vous de ce design ?
+
+
+
+// Que faire ?
+// Séparer ce qui change de ce qui ne change pas
+// prochaine étape : on va créer des classes pour séparer les comportements de la classe Canard
