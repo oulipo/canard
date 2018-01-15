03d4919ce2d1f39f524eb613fac388a74ee5e75d
laurentws <laurent@websuit.fr>
2018-01-15 21:10:49 +0100

    de nouveaux canards

--- Canard.php
+++ Canard.php
@@ -11,6 +11,10 @@ abstract class Canard {
         echo "Je nage...\n";
     }
 
+    public function vole() { // tous les canards volent désormais...
+        echo "Je vole...\n";
+    }
+
     abstract protected function affiche();
 }
 
@@ -26,9 +30,28 @@ class TeteRouge extends Canard {
     }
 }
 
+// mais un autre développeur ajoute la classe CanardEnCaoutchouc...
+class CanardEnCaoutchouc extends Canard {
+    public function affiche() {
+        echo "Je suis un canard en caoutchouc\n";
+    }
+    public function cancane() { // on override ici car ce type de canard ne cancane pas pas mais fait couic-couic...
+        echo "couic couic...\n";
+    }
+}
+
 $c1 = new Colvert();
 $c2 = new TeteRouge();
 $c1->affiche();
 $c1->nage();
 $c2->affiche();
 $c2->cancane();
+
+// désormais les canards en caoutchouc peuvent voler...
+$c3 = new CanardEnCaoutchouc();
+$c3->vole();
+// L'héritage semble être une bonne idée pour réutiliser le code mais cela devient problématique la maintenance et l'évolution du projet devient plus compliquée...
+// on va devoir overrider vole() pour que ce canard ne vole pas.
+
+// idem si l'on doit ajouter une classe pour les leurres de canard en bois
+// celle-ci ne doit pas voler, ni cancaner...
