232963386ca81dd18b1ef5b1c7764ae22eeab02b
laurentws <laurent@websuit.fr>
2018-01-15 22:41:37 +0100

    de meilleurs comportements

--- Canard.php
+++ Canard.php
@@ -1,15 +1,38 @@
 <?php
 // Simulateur de canard...
 
-interface Volable {
+interface FlyBehavior { // c'est plus simple en anglais...
     public function vole();
 }
 
-interface Cancanable { // néologisme !
-    public function cancane();
+class FlyWithWings implements FlyBehavior {
+    public function vole() {
+        echo "Je vole avec mes ailes...\n";
+    }
+}
+class FlyNoWay implements FlyBehavior {
+    public function vole() {
+        echo "Je ne peux pas voler...\n";
+    }
 }
 
+interface QuackBehavior {
+    public function cancane();
+}
+// exercice : trouver des comportements pour les "quacks" des Colverts, etc.
+
 abstract class Canard {
+
+    protected $flyBehavior;
+    protected $quackBehavior;
+
+    public function performFly() {
+        $this->flyBehavior->vole();
+    }
+
+    public function performQuack() {
+        $this->quackBehavior->cancane();
+    }
 
     public function nage() {
         echo "Je nage...\n";
@@ -18,36 +41,27 @@ abstract class Canard {
     abstract protected function affiche();
 }
 
-class Colvert extends Canard implements Cancanable, Volable {
+class Colvert extends Canard {
+
+    public function __construct() {
+        $this->flyBehavior = new FlyWithWings();
+    }
+
     public function affiche() {
         echo "Je suis un colvert\n";
     }
-    public function vole() {
-        echo "Je vole...\n";
-    }
-    public function cancane() {
-        echo "coin-coin\n";
-    }
+    
 }
 
-class TeteRouge extends Canard implements Cancanable, Volable {
+class TeteRouge extends Canard {
     public function affiche() {
         echo "Je suis un fuligule à tête rouge\n";
     }
-    public function vole() {
-        echo "Je vole...\n";
-    }
-    public function cancane() {
-        echo "coin-coin\n";
-    }
 }
 
-class CanardEnCaoutchouc extends Canard implements Cancanable {
+class CanardEnCaoutchouc extends Canard {
     public function affiche() {
         echo "Je suis un canard en caoutchouc\n";
-    }
-    public function cancane() { 
-        echo "couic couic...\n";
     }
 }
 
@@ -57,15 +71,9 @@ class LeurreDeCanardEnBois extends Canard {
     }
 }
 
+// 1- faire des tests
+// 2- changer dynamiquement le comportement
+// 3- setFlyBehavior(FlyBehavior $fb) { flyBehavior = $fb;  }
+
 $c1 = new Colvert();
-$c2 = new TeteRouge();
-$c3 = new CanardEnCaoutchouc();
-$c4 = new LeurreDeCanardEnBois();
-
-// Que pensez-vous de ce design ?
-
-
-
-// Que faire ?
-// Séparer ce qui change de ce qui ne change pas
-// prochaine étape : on va créer des classes pour séparer les comportements de la classe Canard
+$c1->performFly();
